fa40b82d35bc13e540897f0344c6bc01afcef9f4
斜照 <youming@cai-inc.com>
2021-11-29 14:20:13 +0800

    snapshot branch

---
+++ ares-spring-boot-samples/ares-rest-sample/src/main/java/cn/gov/zcy/ares/sample/rest/RestSampleApplication.java
@@ -0,0 +1,15 @@
+package cn.gov.zcy.ares.sample.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:youming@cai-inc.com">斜照</a>
+ * @datetime 2021-11-29 14:18:25
+ */
+@SpringBootApplication
+public class RestSampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestSampleApplication.class, args);
+    }
+}
